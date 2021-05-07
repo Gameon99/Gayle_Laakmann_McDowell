8319622bbb07d73cd1c75297bb7a284cfb9ba1f8
Sahithi <62688362+Gameon99@users.noreply.github.com>
2021-05-07 19:09:23 +0530

    Create resizableArrays.java

---
+++ Arrays and Strings/resizableArrays.java
@@ -0,0 +1,13 @@
+ArrayLists
+resizable factor : 2
+implements list interface extends dictionary
+ArrayList<Integer> arrayList = new ArrayList<Integer>();
+.add(value);
+.remove(index);
+.size();
+.get(index);
+.set(index,value);
+Amortized complexity = O(1) for each insertion
+.clear();
+.clone();
+.indexOf();
