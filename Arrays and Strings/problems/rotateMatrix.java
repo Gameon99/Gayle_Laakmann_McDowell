b1721d7cb5bb6196ddbcfd8126e4d1b09bdc27f6
Sahithi <62688362+Gameon99@users.noreply.github.com>
2021-05-08 12:37:38 +0530

    incomplete

---
+++ Arrays and Strings/problems/rotateMatrix.java
@@ -0,0 +1,18 @@
+//incomplete
+package GayleLaakmann;
+
+import java.util.*;
+
+public class rotateMatrix {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[][] matrix = new int[N][N];
+        for(int i = 0;i < N;i++){
+            for(int j = 0;j < N;j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        
+    }
+}
