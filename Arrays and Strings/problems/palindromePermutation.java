b3850061554ad85ca94ab152d348f78c4d2e6419
Sahithi <62688362+Gameon99@users.noreply.github.com>
2021-05-08 11:00:40 +0530

    read bit vectors

--- Arrays and Strings/problems/palindromePermutation.java
+++ Arrays and Strings/problems/palindromePermutation.java
@@ -1,4 +1,4 @@
-//Solution - check
+//Solution - check - similar to solution2
 import java.util.*;
 import java.io.*;
 public class Solution{
@@ -26,3 +26,45 @@ public class Solution{
   }
 }
 //hashtable solution - O(N) TC
+package GayleLaakmann;
+import java.util.*;
+public class isPermutation {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int[] table = buildTable(s);
+        int count = 0;
+        for(int i : table){
+            if(i % 2 == 1){
+                count++;
+            }
+        }
+        if(count > 1){
+            System.out.println("Not possible");
+        }else{
+            System.out.println("Possible");
+        }
+    }
+    public static int[] buildTable(String s){
+        int[] table = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+        for(char c : s.toCharArray()){
+            int x = getCharNumber(c);
+            if(x != -1){
+                table[x]++;
+            }
+        }
+        return table;
+    }
+    public static int getCharNumber(char c){
+        int a = Character.getNumericValue('a');
+        int z = Character.getNumericValue('z');
+        int value = Character.getNumericValue(c);
+        if(value >= a && value <= z){
+            return value - a;
+        }
+        return -1;
+    }
+
+}
+//read bitvectors
+    
