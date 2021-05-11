a00003b58ddcee10dd16f99dff6e20ca060bcd9e
Sahithi <62688362+Gameon99@users.noreply.github.com>
2021-05-11 12:53:31 +0530

    Update kthtolast.java

--- Linked Lists/kthtolast.java
+++ Linked Lists/kthtolast.java
@@ -1,3 +1,4 @@
+//recursive solution
 public static int kthtolast(Node node, int k){
 
         if(node == null){
@@ -8,4 +9,18 @@ public static int kthtolast(Node node, int k){
             System.out.println(node.data);
         }
         return index;
+    }
+//iterative solution
+public static int kthtolast(Node node, int k){
+        Node s1 = node;
+        Node s2 = node;
+
+        for(int i = 0;i < k;i++){
+            s1 = s1.next;
+        }
+        while(s1 != null){
+            s1 = s1.next;
+            s2 = s2.next;
+        }
+        return s2.data;
     }
