7d316f0e2130cba8b3f0d1aacacd3a3fff2e6853
Sahithi <62688362+Gameon99@users.noreply.github.com>
2021-05-18 20:42:40 +0530

    Create graphsIntro.java

---
+++ Trees and Graphs/graphsIntro.java
@@ -0,0 +1,7 @@
+A tree is actually a type of graph, but not all graphs are trees. Simply put, a tree is a connected graph without cycles.
+Types : 
+1.Directed
+2.Undirected
+Ways to represent a graph:
+1.Adjacency List
+2.Adjacency Matrix
