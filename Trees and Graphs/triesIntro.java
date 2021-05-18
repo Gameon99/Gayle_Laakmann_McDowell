02e456728102cf4ddab5e90df8caae4239b84e4b
Sahithi <62688362+Gameon99@users.noreply.github.com>
2021-05-18 12:39:15 +0530

    Create triesIntro.java

---
+++ Trees and Graphs/triesIntro.java
@@ -0,0 +1,6 @@
+Trie - Also called prefix tree
+n-ary tree 
+each node - character
+each path - may represent a word
+* node = null node (used to represent end of the word)
+tells us if a string is a prefix of any valid words in  O(k) time, where K is the length of the string.
