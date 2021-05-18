c29f75c046cde1a1d5957a6dbaa9682808653c4b
Sahithi <62688362+Gameon99@users.noreply.github.com>
2021-05-18 10:38:58 +0530

    Create AnimalShelter.java

---
+++ Stacks & Queues/AnimalShelter.java
@@ -0,0 +1,3 @@
+Maintain two different linkedlists to store dogs and cats and a time stamp storing order
+If DeQueueAny is used, time stamp is compared and accordingly given out.
+  Else animal is given out from linkedlist's head;
