b689cab8f738127724c53ebe3de852da3f132e6f
Sahithi <62688362+Gameon99@users.noreply.github.com>
2021-05-30 20:49:39 +0530

    Create shifts.java

---
+++ Bit Manipulation/shifts.java
@@ -0,0 +1,3 @@
+There are two types of right shift operators. 
+The arithmetic right shift essentially divides by two.
+The logical right shift does what we would visually see as shifting the bits. This is best seen on a negative number. 
