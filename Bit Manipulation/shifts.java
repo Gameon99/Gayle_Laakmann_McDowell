6a1ddeb6eabed9152ac01686e7e6a9b02194f34f
Sahithi <62688362+Gameon99@users.noreply.github.com>
2021-06-01 08:49:58 +0530

    Update shifts.java

--- Bit Manipulation/shifts.java
+++ Bit Manipulation/shifts.java
@@ -1,3 +1,19 @@
 There are two types of right shift operators. 
 The arithmetic right shift essentially divides by two.
 The logical right shift does what we would visually see as shifting the bits. This is best seen on a negative number. 
+ 
+Arithmetic Shift :
+int repeatedArithmeticShift(int n, int count){
+  for(int i = 0;i < count;i++){
+    n >>= 1;
+  }
+  return n;
+}
+
+Logical Shift : 
+int repeatedLogicalShift(int n, int count){
+  for(int i = 0;i < count;i++){
+    n >>>= 1;
+  }
+  return n;
+}
